5194b33c32aa1d0dfe0cfa74639a1d8cc420b087
SRatna <adhikariratna52@gmail.com>
2017-05-13 11:24:28 +0545

    moviesServices is provided to movie component

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../classes/movie';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.css']
+  styleUrls: ['./movie.component.css'],
+  providers: [MoviesService]
 })
 export class MovieComponent implements OnInit {
 
-  constructor() { }
+  movies: Movie[];
+
+  constructor(private moviesService: MoviesService) { }
+
+  getMovies(): void {
+    this.moviesService.getMovies().then(movies => this.movies = movies);
+  }
 
   ngOnInit() {
+    this.getMovies();
   }
 
 }
